9031f62dab509c95bf72d5a72b0d28ab4c14de4a
MeghavatiChaudhari <meghapjc29@gmail.com>
2023-08-09 21:17:50 +0530

    all functionalities added

--- index.js
+++ index.js
@@ -15,4 +15,27 @@ function addTask(){
         li.appendChild(span);
     }
     boxinput.value='';
+    saveData();
 }
+
+addlist.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data" , addlist.innerHTML);
+}
+
+function show(){
+    addlist.innerHTML=localStorage.getItem('data');
+}
+show();
